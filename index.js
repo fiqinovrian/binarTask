313ca33859fae66b99d11f58d9109d6bb22273c9
fiqinovrian <fiqinovrian@gmail.com>
2022-06-14 11:44:39 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const { User } = require('./models');
+const port = 3000;
+
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+//route
+app.get('/', (req, res) => {
+    res.render('login');
+})
+app.get('/api/v1/register', (req, res) => {
+    res.render('register')
+})
+app.post('/api/v1/register', (req, res) => {
+    const data = {
+        nama: req.body.nama,
+        tanggalLahir: req.body.tanggalLahir,
+        pekerjaan: req.body.pekerjaan,
+        email: req.body.email,
+        password: req.body.password
+    }
+    User.create(data).then(data => {
+        res.status(201).json(data)
+    }).catch(err => {
+        res.status(500).json(err)
+    })
+})
+// User.create({
+//     nama: 'Fiqi Novrian',
+//     tanggalLahir: '2022-02-02',
+//     pekerjaan: 'Staff',
+//     email: 'fiqinovrian@gmail.com',
+//     password: 'password'
+// }).then(user => {
+//     console.log(user)
+// })
+
+app.listen(port, () => console.log(`Server berjalan di http://localhost:${port}`));
